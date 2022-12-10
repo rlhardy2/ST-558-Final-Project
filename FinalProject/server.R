f92e16a49b9ce9da92fb84f4ce0bc9337b1ecc52
Rachel Hardy <rhardy797@hotmail.com>
2022-12-10 16:09:17 -0500

    Adding some numerical summaries and fixing the wine data set.

--- FinalProject/server.R
+++ FinalProject/server.R
@@ -9,10 +9,19 @@ library(ggplot2)
 library(readr)
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
     
-    output$picture <- renderImage({
-        
+    #Data frame for "Data" tab
+    output$table <- DT::renderDataTable(wine)
+    
+    #Mean summary for "Data Exploration" tab
+    output$summary <- DT::renderDataTable({
+      var <- input$summary
+      tab <- wine %>% 
+        select("type", "quality", var) %>%
+        group_by(type, quality) %>%
+        summarize(mean = round(mean(get(var)), 2), sd = round(sd(get(var)), 2))
+      tab
     })
 
 })
--- FinalProject/ui.R
+++ FinalProject/ui.R
@@ -8,16 +8,31 @@ library(tidyverse)
 library(ggplot2)
 library(readr)
 
-red <- read_csv2("winequality-red.csv")
-white <- read_csv2("winequality-white.csv")
+red <- read_csv2("winequality-red.csv") %>% mutate(type = "red") %>% select(type, everything())
+white <- read_csv2("winequality-white.csv") %>% mutate(type = "white") %>% select(type, everything())
+wine <- rbind(red, white)
+
+wine <- wine %>% rename(fixed_acidity = `fixed acidity`,
+                        volatile_acidity = `volatile acidity`,
+                        citric_acid = `citric acid`,
+                        residual_sugar = `residual sugar`,
+                        free_sulfur_dioxide = `free sulfur dioxide`,
+                        total_sulfur_dioxide = `total sulfur dioxide`)
+
+wine$volatile_acidity <- as.numeric(wine$volatile_acidity)
+wine$citric_acid <- as.numeric(wine$citric_acid)
+wine$residual_sugar <- as.numeric(wine$residual_sugar)
+wine$chlorides <- as.numeric(wine$chlorides)
+wine$density <- as.numeric(wine$density)
+wine$sulphates <- as.numeric(wine$sulphates)
 
 dashboardPage(skin = "blue",
     
     #Add a title
-    dashboardHeader(title = "Wine Quality"),
+    dashboardHeader(title = "Wine Quality App"),
     
     #Create sidebar tabs with icons
-    dashboardSidebar(sidebarMenu(
+    dashboardSidebar(sidebarMenu(id = "sidebar",
         menuItem("About", tabName = "about", icon = icon("archive")),
         menuItem("Data", tabName = "data", icon = icon("folder-open")),
         menuItem("Data Exploration", tabName = "exploration", icon = icon("calculator")),
@@ -28,9 +43,9 @@ dashboardPage(skin = "blue",
     dashboardBody(
         tabItems(
             
-            #First tab content
+            #First tab content - About
             tabItem(tabName = "about",
-                    h2("Wine Quality App - Information"),
+                    h2(strong("Wine Quality App - Information")),
                     br(),
                     h4("About the app..."),
                     br(),
@@ -38,12 +53,44 @@ dashboardPage(skin = "blue",
                     br(),
                     h4("Put image here somehow.")),
             
+            #Second tab content - Data
             tabItem(tabName = "data",
-                    h2("Wine Quality App - Data"))
+                    h2(strong("Wine Quality App - Data")),
+                    br(),
+                    fluidRow(
+                        column(width = 6,
+                               box(width = 12,
+                                   title = strong("Subsetting rows"),
+                                   background = "light-blue",
+                                   selectInput(inputId = "wine_type", label = "Wine Type",
+                                               choices = c("red", "white"), selected = "red"))),
+                        column(width = 6,
+                               box(width = 12,
+                                   title = strong("Subsetting columns"),
+                                   background = "light-blue"))
+                    ),
+                    DT::dataTableOutput("table")),
             
+            #Third tab content - Data Exploration
+            tabItem(tabName = "exploration",
+                    h2(strong("Wine Quality App - Data Exploration"),
+                    br()),
+                    fluidRow(
+                      column(width = 6,
+                             box(width = 12,
+                                 title = strong("Summarizing variables (mean and standard deviation)"),
+                                 background = "light-blue",
+                                 selectInput(inputId = "summary", label = "Variables to Summarize",
+                                             choices = c("fixed_acidity", "volatile_acidity", "citric_acid",
+                                                         "residual_sugar", "chlorides", "density",
+                                                         "pH", "sulphates", "alcohol", "free_sulfur_dioxide",
+                                                         "total_sulfur_dioxide"), 
+                                             selected = "fixed_acidity")))),
+                    DT::dataTableOutput("summary"))
         )
     )
 )
+
 
 
 
